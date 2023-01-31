a200e9585c98297445a08cafa9e0f2ef703df36d
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-01-31 15:23:48 +0300

    statefull user sign up page

---
+++ src/UserSignupPage.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+class UserSignupPage extends React.Component {
+
+    state = {
+        userName: null,
+        displayName: null,
+        password: null,
+        passwordRepeat: null
+    };
+
+    onChange = event => {
+        //object destructuring
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+
+
+    render() {
+        return (
+            <form>
+                <h1>Sign Up</h1>
+                <div>
+                    <label>User Name: </label>
+                    <input name="userName" onChange={this.onChange} />
+                </div>
+
+                <div>
+                    <label>Display Name: </label>
+                    <input name="displayName" onChange={this.onChange} />
+                </div>
+
+                <div>
+                    <label>Password: </label>
+                    <input name="password" type="password" onChange={this.onChange} />
+                </div>
+
+                <div>
+                    <label>Password Repeat: </label>
+                    <input name="passwordRepeat" type="password" onChange={this.onChange} />
+                </div>
+
+                <button>Sign Up</button>
+            </form>
+        );
+    }
+}
+export default UserSignupPage;
