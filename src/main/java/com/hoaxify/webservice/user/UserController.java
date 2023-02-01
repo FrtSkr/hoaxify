5af367bcfb7cf152fddd48e8781fb3178987fce4
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-01 15:25:50 +0300

    createUser method ok

--- src/main/java/com/hoaxify/webservice/user/UserController.java
+++ src/main/java/com/hoaxify/webservice/user/UserController.java
@@ -1,7 +1,7 @@
 package com.hoaxify.webservice.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +11,10 @@ public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
+    @Autowired
+    UserRepository userRepository;
     @PostMapping("/api/1.0/users")
     public void createUser(@RequestBody User user){
-        log.info(user.toString());
+        userRepository.save(user);
     }
 }
