09d79dcbf5f7b492cb762be934565e9c350746c7
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-01 14:25:41 +0300

    CrossOrigin annotation for COR exception

--- src/main/java/com/hoaxify/webservice/user/UserController.java
+++ src/main/java/com/hoaxify/webservice/user/UserController.java
@@ -1,6 +1,7 @@
 package com.hoaxify.webservice.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
+    @CrossOrigin
     @PostMapping("/api/1.0/users")
     public void createUser(@RequestBody User user){
         log.info(user.toString());
