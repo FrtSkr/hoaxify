e522f31e6cc4c65e1fbb1894f7dbefa8092dcae1
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-13 01:19:54 +0300

    username unique validator

---
+++ backend/src/main/java/com/hoaxify/webservice/user/UniqueUserNameValidator.java
@@ -0,0 +1,20 @@
+package com.hoaxify.webservice.user;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UniqueUserNameValidator implements ConstraintValidator<UniqueUserName , String> {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public boolean isValid(String username, ConstraintValidatorContext context) {
+        User user = userRepository.findByUserName(username);
+        if(user != null){
+            return false;
+        }
+        return true;
+    }
+}
