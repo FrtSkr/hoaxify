e560c0b2b553be376307e95219df5debae9b6e3f
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-13 01:22:09 +0300

    added query method for username

--- backend/src/main/java/com/hoaxify/webservice/user/UserRepository.java
+++ backend/src/main/java/com/hoaxify/webservice/user/UserRepository.java
@@ -3,5 +3,5 @@ package com.hoaxify.webservice.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    public User findByUserName(String username);
 }
