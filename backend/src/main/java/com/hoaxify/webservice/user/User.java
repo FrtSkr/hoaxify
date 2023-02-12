21c05b3a31276fba279cedd5cd7ec8287be2e5aa
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-13 01:20:58 +0300

    added uniqueusername annotation for username

--- backend/src/main/java/com/hoaxify/webservice/user/User.java
+++ backend/src/main/java/com/hoaxify/webservice/user/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -19,6 +20,7 @@ public class User {
     private long id;
     @NotNull
     @Size(min = 4, max = 255)
+    @UniqueUserName
     private String userName;
     @NotNull
     @Size(min = 4, max = 255)
