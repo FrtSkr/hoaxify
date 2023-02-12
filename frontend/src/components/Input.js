ac8d3e486e18355ea4b7387ea412c1b4ef99111f
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-13 01:59:52 +0300

    added type prop

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Input = (props) => {
-    const { error, inputName, label, onChange } = props;
+    const { error, inputName, label, onChange, inputType } = props;
     const className = error ? "form-control is-invalid" : "form-control";
+    const type = inputType ? inputType : "text";
     return (
         <div className="form-group">
             <label>{label}</label>
-            <input className={className} name={inputName} onChange={onChange} />
+            <input className={className} name={inputName} onChange={onChange} type={type} />
             <div className="invalid-feedback">
                 {error}
             </div>
