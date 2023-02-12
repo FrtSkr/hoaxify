af58e442eb10b4264254072237dcb43fb3bbaddc
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-13 00:13:52 +0300

    user validation conditions

--- backend/src/main/java/com/hoaxify/webservice/user/User.java
+++ backend/src/main/java/com/hoaxify/webservice/user/User.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -16,10 +18,14 @@ public class User {
     @GeneratedValue
     private long id;
     @NotNull
+    @Size(min = 4, max = 255)
     private String userName;
     @NotNull
+    @Size(min = 4, max = 255)
     private String displayName;
     @NotNull
+    @Size(min = 8, max = 255)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
 
 }
