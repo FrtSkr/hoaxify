e61aeae605ce3a589abbfef88f907943607c39a4
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-14 01:55:09 +0300

    multi language support : translation

--- backend/src/main/java/com/hoaxify/webservice/user/User.java
+++ backend/src/main/java/com/hoaxify/webservice/user/User.java
@@ -1,11 +1,9 @@
 package com.hoaxify.webservice.user;
 
-import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -18,16 +16,16 @@ public class User {
     @Id
     @GeneratedValue
     private long id;
-    @NotNull
-    @Size(min = 4, max = 255)
-    @UniqueUserName
+    @NotNull(message = "{hoaxify.constraint.username.NotNull.message}")
+    @Size(min = 4, max = 255, message = "{hoaxify.constraint.Size.message}")
+    @UniqueUserName(message = "{hoaxify.constraint.UniqueUserName.message}")
     private String userName;
-    @NotNull
-    @Size(min = 4, max = 255)
+    @NotNull(message = "{hoaxify.constraint.displayName.NotNull.message}")
+    @Size(min = 4, max = 255, message = "{hoaxify.constraint.Size.message}")
     private String displayName;
-    @NotNull
-    @Size(min = 8, max = 255)
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+    @NotNull(message = "{hoaxify.constraint.password.NotNull.message}")
+    @Size(min = 8, max = 255, message = "{hoaxify.constraint.Size.message}")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{hoaxify.constraint.password.Pattern.message}")
     private String password;
 
 }
