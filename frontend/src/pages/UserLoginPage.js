94a3ba8f1443378b64565ef5ccc6a145f052177c
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-15 13:40:36 +0300

    created user login page

---
+++ frontend/src/pages/UserLoginPage.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Input from "../components/Input";
+import { withTranslation } from "react-i18next";
+
+class UserLoginPage extends React.Component {
+
+    state = {
+        userName: null,
+        password: null,
+        errors: {},
+        pendingApiCall: false
+
+    }
+
+    onChange = event => {
+        const { name, value } = event.target;
+        const errors = { ...this.state.errors };
+        errors[name] = undefined;
+        this.setState({
+            [name]: value,
+            errors
+        });
+
+    }
+
+
+    onClickSignUp = async event => {
+        event.preventDefault();
+
+        const { userName, password } = this.state;
+
+        const body = {
+            userName,
+            password
+        };
+        // TODO: HTTP Request
+
+    };
+
+
+    render() {
+        const { userName, password } = this.state.errors;
+        const { pendingApiCall } = this.state;
+        const { t } = this.props;
+        return (
+            <div className="container">
+                <form>
+                    <h1 className="text-center">{t('Login')}</h1>
+                    <Input label={t("Username")} error={userName} inputName="userName" onChange={this.onChange} />
+                    <Input label={t("Password")} error={password} inputName="password" onChange={this.onChange} inputType="password" />
+
+                    <div className="text-center">
+                        <button className="btn btn-primary"
+                            onClick={this.onClickSignUp}
+                            disabled={pendingApiCall}>
+                            {pendingApiCall &&
+                                <span className="spinner-border spinner-border-sm"></span>}
+                            {t('Login')}</button>
+                    </div>
+                </form>
+            </div>
+        );
+    };
+
+}
+export default withTranslation()(UserLoginPage);
