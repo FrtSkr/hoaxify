4a7ceea07cc293f28093adb6b012b5183bef01d9
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-16 01:45:35 +0300

    added default user

--- backend/src/main/java/com/hoaxify/webservice/WebServiceApplication.java
+++ backend/src/main/java/com/hoaxify/webservice/WebServiceApplication.java
@@ -1,14 +1,30 @@
 package com.hoaxify.webservice;
 
+import com.hoaxify.webservice.user.User;
+import com.hoaxify.webservice.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WebServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServiceApplication.class, args);
+
+	}
+
+	@Bean
+	CommandLineRunner createInitialUsers(UserService userService){
+		return (args) -> {
+			User user = new User();
+			user.setUserName("admin");
+			user.setDisplayName("admin");
+			user.setPassword("Admin123");
+			userService.save(user);
+		};
 	}
 
 }
