43a5a6bcb06bf1038a5f24329f80a8be616dcfb0
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-15 12:12:07 +0300

    dynamic accept-language

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -1,6 +1,12 @@
-import axios from "axios";
+import axios from "axios"; // for HTTP request
 
 export const signup = (body) => {
     const url = '/api/1.0/users';
     return axios.post(url, body);
+};
+
+
+export const changeLanguage = language => {
+    console.log(language);
+    axios.defaults.headers['accept-language'] = language;
 };
