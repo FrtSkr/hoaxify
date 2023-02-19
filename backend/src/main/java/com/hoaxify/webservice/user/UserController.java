2b473a97d20b5c47d9e9e255f2cda182897ece37
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-19 18:07:41 +0300

    refactor for error handler

--- backend/src/main/java/com/hoaxify/webservice/user/UserController.java
+++ backend/src/main/java/com/hoaxify/webservice/user/UserController.java
@@ -1,17 +1,9 @@
 package com.hoaxify.webservice.user;
-import com.hoaxify.webservice.error.ApiError;
 import com.hoaxify.webservice.shared.GenericResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class UserController {
@@ -24,18 +16,6 @@ public class UserController {
         return new GenericResponse("user created");
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleValidationException(MethodArgumentNotValidException exception){
-        ApiError apiError = new ApiError(400, "validation error", "/api/1.0/users");
-        Map<String, String> validationError = new HashMap<>();
-
-        for(FieldError fieldError : exception.getBindingResult().getFieldErrors()){
-            validationError.put(fieldError.getField(), fieldError.getDefaultMessage());
-        }
-        apiError.setValidationErrors(validationError);
-        return  apiError;
-    }
 }
 
 
