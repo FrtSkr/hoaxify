9d9b2f7088f5aa4bb7f5c33a7a8a23108077739d
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-19 18:06:27 +0300

    error handler for all error cases

--- backend/src/main/java/com/hoaxify/webservice/error/ErrorHandler.java
+++ backend/src/main/java/com/hoaxify/webservice/error/ErrorHandler.java
@@ -1,14 +1,16 @@
 package com.hoaxify.webservice.error;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -20,12 +22,29 @@ public class ErrorHandler implements ErrorController {
 
     @RequestMapping("/error")
     ApiError handleError(WebRequest webRequest){
-        Map<String, Object> attributes = this.errorAttributes.getErrorAttributes(webRequest, ErrorAttributeOptions.defaults());
+
+        Map<String, Object> attributes = this.errorAttributes.getErrorAttributes(
+                webRequest,
+                ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE,
+                        ErrorAttributeOptions.Include.BINDING_ERRORS));
+
         String message = (String) attributes.get("message");
         String path = (String) attributes.get("path");
         int status = (Integer) attributes.get("status");
+        System.out.println("messaga: "+ message);
+        ApiError apiError = new ApiError(status, message, path);
 
-        return new ApiError(status, message, path);
+        if(attributes.containsKey("errors")){
+            List<FieldError> fieldErrors = (List<FieldError>)attributes.get("errors");
+            Map<String, String> validationError = new HashMap<>();
+
+            for(FieldError fieldError : fieldErrors){
+                validationError.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+            apiError.setValidationErrors(validationError);
+        }
+
+        return apiError;
     }
 
 
