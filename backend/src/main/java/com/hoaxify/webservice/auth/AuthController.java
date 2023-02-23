7d53569d340d0abb8ff847a714bad023d0db5e28
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-23 21:15:55 +0300

    refactor success response

--- backend/src/main/java/com/hoaxify/webservice/auth/AuthController.java
+++ backend/src/main/java/com/hoaxify/webservice/auth/AuthController.java
@@ -1,35 +1,24 @@
 package com.hoaxify.webservice.auth;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.hoaxify.webservice.shared.CurrentUser;
 import com.hoaxify.webservice.shared.Views;
 import com.hoaxify.webservice.user.User;
 import com.hoaxify.webservice.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Base64;
 
 @RestController
 public class AuthController {
     final String basePath = "/api/1.0/auth";
-    @Autowired
-    UserRepository userRepository;
 
     @PostMapping(basePath)
     @JsonView(Views.Base.class)
-    ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization") String authorization){
-        String base64Encoded = authorization.split("Basic ")[1];
-        String decoded = new String(Base64.getDecoder().decode(base64Encoded));
-        String[] parts = decoded.split(":");
-        String username = parts[0];
-        User inDB = userRepository.findByUsername(username);
-
-        return ResponseEntity.ok(inDB);
-
-
+    ResponseEntity<?> handleAuthentication(@CurrentUser User user){
+        return ResponseEntity.ok(user);
     }
 
 }
