8409b9c846446f13984413088fa202edf7f251d5
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-24 12:58:57 +0300

    added generalized progress

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,12 +7,15 @@ import UserSignupPage from './pages/UserSignupPage';
 import UserLoginPage from './pages/UserLoginPage';
 import './i18n';
 import LanguageSelector from './components/LanguageSelector';
+import ApiProgress from './shared/ApiProgress';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <UserLoginPage />
+      <ApiProgress>
+        <UserSignupPage />
+      </ApiProgress>
       <LanguageSelector />
     </div>
   </React.StrictMode>
