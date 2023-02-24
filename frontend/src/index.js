68738f2ff44c878cdf96becb798b38f14c8dc1ac
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-02-25 00:03:32 +0300

    added App

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './bootstrap-override.scss';
 import reportWebVitals from './reportWebVitals';
-import UserSignupPage from './pages/UserSignupPage';
-import UserLoginPage from './pages/UserLoginPage';
 import './i18n';
-import LanguageSelector from './components/LanguageSelector';
-import ApiProgress from './shared/ApiProgress';
-
+import App from './container/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <ApiProgress>
-        <UserSignupPage />
-      </ApiProgress>
-      <LanguageSelector />
-    </div>
+    <App />
   </React.StrictMode>
 );
 
