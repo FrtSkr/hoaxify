7ff5ad58ad7d25a6b5bd444310abea261b52c514
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-03 00:48:58 +0300

    manage axios interceptor with componentWillUnmount

--- frontend/src/shared/ApiProgress.js
+++ frontend/src/shared/ApiProgress.js
@@ -13,18 +13,24 @@ export function withApiProgress(WrappedComponent, apiPath) {
         }
 
         componentDidMount() {
-            axios.interceptors.request.use(request => {
+            this.requestInterceptor = axios.interceptors.request.use(request => {
                 this.updateApiCallFor(request.url, true)
                 return request;
             });
 
-            axios.interceptors.response.use(response => {
+            this.responseInterceptor = axios.interceptors.response.use(response => {
                 this.updateApiCallFor(response.config.url, false)
                 return response;
             }, error => {
                 this.updateApiCallFor(error.config.url, false)
                 throw error;
             });
+        }
+
+        componentWillUnmount() {
+            const { request, response } = axios.interceptors
+            request.eject(this.requestInterceptor);
+            response.eject(this.responseInterceptor);
         }
 
         updateApiCallFor = (url, inProgress) => {
