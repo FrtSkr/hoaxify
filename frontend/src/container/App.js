cdcd031fe5db82ac9a2e7ad4be6f0e7ea2de6da8
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-02 13:48:12 +0300

    page router

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -3,17 +3,23 @@ import ApiProgress from "../shared/ApiProgress";
 import UserLoginPage from "../pages/UserLoginPage";
 import UserSignupPage from '../pages/UserSignupPage';
 import LanguageSelector from "../components/LanguageSelector";
+import HomePages from "../pages/HomePages";
+import UserPage from "../pages/UserPage";
+import { HashRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="row">
-      <div className="col">
-        <UserSignupPage />
-      </div>
-      <div className="col">
-        <UserLoginPage />
-      </div>
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={HomePages} />
+          <Route path="/login" component={UserLoginPage} />
+          <Route path="/signup" component={UserSignupPage} />
+          <Route path="/user/:username" component={UserPage} />
+          <Redirect to="/" />
+        </Switch>
+      </HashRouter>
       <LanguageSelector />
     </div>
   );
