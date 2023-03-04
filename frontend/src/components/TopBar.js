1897706f1a103e0f653e68d81992b5e47ee8e1a1
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-04 21:46:00 +0300

    dynamic topbar

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -5,8 +5,49 @@ import { Link } from "react-router-dom";
 import logo from '../assets/hoaxify.png';
 
 class TopBar extends Component {
+
+    state = {
+        isLoggedIn: false,
+        username: 'user1'
+    }
+
+
+
+
     render() {
         const { t } = this.props;
+        const { isLoggedIn, username } = this.state;
+
+        let links = (
+            <ul className="navbar-nav ml-auto">
+                <li>
+                    <Link className='nav-link' to='/login'>
+                        {t('Login')}
+                    </Link>
+                </li>
+                <li>
+                    <Link className='nav-link' to='/signup'>
+                        {t('Sign Up')}
+                    </Link>
+                </li>
+            </ul>
+        );
+
+        this.setState({ isLoggedIn: this.props.isLoggedIn })
+        if (isLoggedIn) {
+            links = (<ul className="navbar-nav ml-auto">
+                <li>
+                    <Link className='nav-link' to={`/user/${username}`}>
+                        {username}
+                    </Link>
+                </li>
+                <li>
+                    <Link className='nav-link'>
+                        {t('Logout')}
+                    </Link>
+                </li>
+            </ul>)
+        }
         return (
             <div className="shadow-sm bg-light mb-2">
                 <nav className="navbar navbar-light container navbar-expand">
@@ -15,18 +56,7 @@ class TopBar extends Component {
                         Hoaxify
                     </Link>
 
-                    <ul className="navbar-nav ml-auto">
-                        <li>
-                            <Link className='nav-link' to='/login'>
-                                {t('Login')}
-                            </Link>
-                        </li>
-                        <li>
-                            <Link className='nav-link' to='/signup'>
-                                {t('Sign Up')}
-                            </Link>
-                        </li>
-                    </ul>
+                    {links}
                 </nav>
             </div>
         );
