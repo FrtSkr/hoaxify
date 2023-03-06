86f2042e39eb300bc470a344c57e1f42a73faa4d
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-06 22:40:07 +0300

    refactor for redux

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -4,22 +4,15 @@ import { withTranslation } from 'react-i18next';
 import { Link } from "react-router-dom";
 import logo from '../assets/hoaxify.png';
 import { connect } from 'react-redux';
+import { logoutSuccess } from '../redux/authActions';
 // import { Authentication } from '../shared/AuthenticationContext';
 
 class TopBar extends Component {
 
     //  static contextType = Authentication;
 
-    onClickLogout = () => {
-        const action = {
-            type: 'logout-success'
-        };
-        this.props.dispatch(action);
-    };
-
     render() {
-        const { t, isLoggedIn, username } = this.props;
-        const onLogoutSuccess = () => { }
+        const { t, isLoggedIn, username, onLogoutSuccess } = this.props;
         let links = (
             <ul className="navbar-nav ml-auto">
                 <li>
@@ -43,7 +36,7 @@ class TopBar extends Component {
                     </Link>
                 </li>
 
-                <li className='nav-link' onClick={this.onClickLogout} style={{ cursor: 'pointer' }}>
+                <li className='nav-link' onClick={onLogoutSuccess} style={{ cursor: 'pointer' }}>
                     {t('Logout')}
                 </li>
 
@@ -72,7 +65,14 @@ const mapStateToProps = store => {
         isLoggedIn: store.isLoggedIn,
         username: store.username
     };
-}
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onLogoutSuccess: () => dispatch(logoutSuccess())
+
+    };
+};
 
 const TopBarWithTranslation = withTranslation()(TopBar);
-export default connect(mapStateToProps)(TopBarWithTranslation);
+export default connect(mapStateToProps, mapDispatchToProps)(TopBarWithTranslation);
