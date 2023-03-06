a76801a7923940810e6269b439ae30b0004131b9
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-06 22:03:43 +0300

    state management with redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,43 @@ import './bootstrap-override.scss';
 import reportWebVitals from './reportWebVitals';
 import './i18n';
 import App from './container/App';
-import AuthenticationContext from './shared/AuthenticationContext';
+// import AuthenticationContext from './shared/AuthenticationContext';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
+
+
+const loggedInState = {
+  isLoggedIn: true,
+  username: "admin",
+  displayName: "displayAdmin",
+  image: null,
+  password: "Admin"
+}
+
+const defaultState = {
+  isLoggedIn: false,
+  username: undefined,
+  displayName: undefined,
+  image: undefined,
+  password: undefined
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <AuthenticationContext>
-    <App />
 
-  </AuthenticationContext>
+const reducer = (state = { ...defaultState }, action) => {
+  if (action.type === 'logout-success') {
+    console.log(action.type);
+    return defaultState;
+  }
+  return state;
+};
+
+const store = createStore(reducer, loggedInState);
+root.render(
+  <Provider store={store}>
+
+    <App />
+  </Provider>
 );
 
 reportWebVitals();
