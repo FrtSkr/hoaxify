e547831d85a8551631022244f3bee9d5b4fcb28d
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-07 21:53:26 +0300

    saving logged in user to local storage

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,16 +1,35 @@
 import { legacy_createStore as createStore } from 'redux';
 import authReducer from './authReducer';
 
-const loggedInState = {
-    isLoggedIn: false,
-    username: undefined,
-    displayName: undefined,
-    image: undefined,
-    password: undefined
-}
+
 
 const configureStore = () => {
-    return createStore(authReducer, loggedInState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+    let loggedInState = {
+        isLoggedIn: false,
+        username: undefined,
+        displayName: undefined,
+        image: undefined,
+        password: undefined
+    }
+
+    const hoaxAuth = localStorage.getItem('hoax-auth');
+
+    if (hoaxAuth) {
+        try {
+            loggedInState = JSON.parse(hoaxAuth)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const store = createStore(authReducer, loggedInState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+    store.subscribe(() => {
+        localStorage.setItem('hoax-auth', JSON.stringify(store.getState()));
+    })
+
+    return store;
 }
 
 export default configureStore;
