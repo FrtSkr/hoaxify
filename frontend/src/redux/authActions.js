a26007c45bebe7aed894d74d8a55faa1bc1bf0f6
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-07 23:02:37 +0300

    added loginHandler for middleware

--- frontend/src/redux/authActions.js
+++ frontend/src/redux/authActions.js
@@ -1,4 +1,5 @@
 import * as ACTIONS from './Constants';
+import { login } from '../api/apiCalls';
 
 export const logoutSuccess = () => {
     return {
@@ -10,5 +11,18 @@ export const loginSuccess = (authState) => {
     return {
         type: ACTIONS.LOGIN_SUCCES,
         payload: authState
+    };
+};
+
+export const loginHandler = credentials => {
+    return async function (dispatch) {
+        const response = await login(credentials);
+        const authState = {
+            ...response.data,
+            password: credentials.password,
+            isLoggedIn: true
+        };
+        dispatch(loginSuccess(authState));
+        return response;
     };
 };
