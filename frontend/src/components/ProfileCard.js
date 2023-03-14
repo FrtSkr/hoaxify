9c590c2d4a1c7308acf330465d590ac4a15801da
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-14 19:19:58 +0300

    refactor for useSelector and useParams

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-const ProfileCard = (props) => {
-    const pathUsername = props.match.params.username;
-    let message = "We cannot edit";
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+const ProfileCard = props => {
+    const { username: loggedInUsername } = useSelector(store => ({ username: store.username }));
+    const routeParams = useParams();
+    const pathUsername = routeParams.username;
 
-    if (pathUsername === props.loggedInUsername) {
+    let message = "We cannot edit";
+    if (pathUsername === loggedInUsername) {
         message = "We can edit";
     }
 
@@ -16,11 +18,4 @@ const ProfileCard = (props) => {
     );
 };
 
-const mapStateToProps = store => {
-    return {
-        loggedInUsername: store.username
-    };
-};
-
-const ProfileCardWithRouter = withRouter(ProfileCard);
-export default connect(mapStateToProps)(ProfileCardWithRouter);
+export default ProfileCard;
