2e7c86daf9ec3c899b2dc4454b7356924d9464e3
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-14 19:17:23 +0300

    refactor for useDispatch, useTranslation and useSelector

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -1,78 +1,72 @@
 
 import React, { Component } from 'react';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { Link } from "react-router-dom";
 import logo from '../assets/hoaxify.png';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logoutSuccess } from '../redux/authActions';
-// import { Authentication } from '../shared/AuthenticationContext';
 
-class TopBar extends Component {
+const TopBar = props => {
 
-    //  static contextType = Authentication;
+    const { t } = useTranslation();
+    const { isLoggedIn, username } = useSelector(store => ({
 
-    render() {
-        const { t, isLoggedIn, username, onLogoutSuccess } = this.props;
-        let links = (
-            <ul className="navbar-nav ml-auto">
-                <li>
-                    <Link className='nav-link' to='/login'>
-                        {t('Login')}
-                    </Link>
-                </li>
-                <li>
-                    <Link className='nav-link' to='/signup'>
-                        {t('Sign Up')}
-                    </Link>
-                </li>
-            </ul>
-        );
-
-        if (isLoggedIn) {
-            links = (<ul className="navbar-nav ml-auto">
-                <li>
-                    <Link className='nav-link' to={`/user/${username}`}>
-                        {username}
-                    </Link>
-                </li>
-
-                <li className='nav-link' onClick={onLogoutSuccess} style={{ cursor: 'pointer' }}>
-                    {t('Logout')}
-                </li>
-
-            </ul>)
-        }
-        return (
-            <div className="shadow-sm bg-light mb-2">
-                <nav className="navbar navbar-light container navbar-expand">
-                    <Link className="navbar-brand" to="/">
-                        <img src={logo} width="60" alt="Hoaxify Logo" />
-                        Hoaxify
-                    </Link>
-
-                    {links}
-                </nav>
-            </div>
-        );
-
-
-
-    }
-}
-
-const mapStateToProps = store => {
-    return {
         isLoggedIn: store.isLoggedIn,
         username: store.username
-    };
-};
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onLogoutSuccess: () => dispatch(logoutSuccess())
+    }));
+    const dispatch = useDispatch();
+    const onLogoutSuccess = () => {
+        dispatch(logoutSuccess());
+    }
+    let links = (
+        <ul className="navbar-nav ml-auto">
+            <li>
+                <Link className='nav-link' to='/login'>
+                    {t('Login')}
+                </Link>
+            </li>
+            <li>
+                <Link className='nav-link' to='/signup'>
+                    {t('Sign Up')}
+                </Link>
+            </li>
+        </ul>
+    );
 
-    };
-};
+    if (isLoggedIn) {
+        links = (<ul className="navbar-nav ml-auto">
+            <li>
+                <Link className='nav-link' to={`/user/${username}`}>
+                    {username}
+                </Link>
+            </li>
 
-const TopBarWithTranslation = withTranslation()(TopBar);
-export default connect(mapStateToProps, mapDispatchToProps)(TopBarWithTranslation);
+            <li className='nav-link' onClick={onLogoutSuccess} style={{ cursor: 'pointer' }}>
+                {t('Logout')}
+            </li>
+
+        </ul>)
+    }
+    return (
+        <div className="shadow-sm bg-light mb-2">
+            <nav className="navbar navbar-light container navbar-expand">
+                <Link className="navbar-brand" to="/">
+                    <img src={logo} width="60" alt="Hoaxify Logo" />
+                    Hoaxify
+                </Link>
+
+                {links}
+            </nav>
+        </div>
+    );
+
+
+
+
+}
+
+
+
+
+export default TopBar;
