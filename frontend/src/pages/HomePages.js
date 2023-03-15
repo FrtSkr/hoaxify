8522673ef022a8d5dcf2b0ed94a03551a775d654
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-15 20:21:15 +0300

    added UserList component

--- frontend/src/pages/HomePages.js
+++ frontend/src/pages/HomePages.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import UserList from '../components/UserList';
 const HomePages = () => {
     return (
         <div className='container'>
-            HomePages
+            <UserList />
         </div>
     );
 };
