ab06ed1843bcfb8a661ecfa33986d1e8b842e028
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-16 23:33:42 +0300

    getAllUsersProjection method commented

--- backend/src/main/java/com/hoaxify/webservice/user/UserRepository.java
+++ backend/src/main/java/com/hoaxify/webservice/user/UserRepository.java
@@ -1,7 +1,14 @@
 package com.hoaxify.webservice.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
      User findByUsername(String username);
+
+     // UserProjection için kullanılan metot. Ama biz farklı bir yöntem uygulayacağız.
+//     @Query(value = "Select u from User u")
+//     Page<UserProjection> getAllUsersProjection(Pageable page);
 }
