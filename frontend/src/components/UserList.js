e53c270a33c8cdbc9a66009353625eaa9a001e7d
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-17 22:23:53 +0300

    fixed for functional component

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,63 +1,57 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getUsers } from '../api/apiCalls';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import UserListItem from './UserListItem';
-class UserList extends Component {
 
-    state = {
-        page: {
-            content: [],
-            number: 0,
-            size: 3
-        }
+const UserList = () => {
+
+    const [page, setPage] = useState({
+        content: [],
+        number: 0,
+        size: 3
+    });
+
+    useEffect(() => {
+        loadUsers();
+    }, []);
+
+    const onClickNext = () => {
+        const nextPage = page.number + 1;
+        loadUsers(nextPage);
     }
 
-    componentDidMount() {
-        this.loadUsers();
-
-    };
-
-    onClickNext = () => {
-        const nextPage = this.state.page.number + 1;
-        this.loadUsers(nextPage);
+    const onClickPrevious = () => {
+        const previousPage = page.number - 1;
+        loadUsers(previousPage);
     }
 
-    onClickPrevious = () => {
-        const nextPage = this.state.page.number - 1;
-        this.loadUsers(nextPage);
-    }
-
-    loadUsers = page => {
+    const loadUsers = page => {
         getUsers(page).then(response => {
-            this.setState({
-                page: response.data
-            });
+            setPage(response.data);
         });
     }
 
-    render() {
-        const { content: users, last, first } = this.state.page;
-        const { t } = this.props;
-        return (
+    const { content: users, last, first } = page;
+    const { t } = useTranslation();
+    return (
 
-            <div className='card'>
-                <h3 className='card-header text-center'>{t('Users')}</h3>
-                <div className='list-group'>
-                    {
-                        users.map(user => (
-                            <UserListItem key={user.username} user={user} />
-                        ))
-                    }
-                </div>
-                <div>
-                    {first === false && (<button className='btn btn-sm btn-light' onClick={this.onClickPrevious}>{t('Back')}</button>)}
-
-                    {last === false && (<button className='btn btn-sm btn-light float-right' onClick={this.onClickNext}>{t('Next')}</button>)}
-                </div>
+        <div className='card'>
+            <h3 className='card-header text-center'>{t('Users')}</h3>
+            <div className='list-group'>
+                {
+                    users.map(user => (
+                        <UserListItem key={user.username} user={user} />
+                    ))
+                }
             </div>
-        );
-    };
-}
-const UserListWithTranslation = withTranslation()(UserList);
+            <div>
+                {first === false && (<button className='btn btn-sm btn-light' onClick={onClickPrevious}>{t('Back')}</button>)}
 
-export default UserListWithTranslation;
+                {last === false && (<button className='btn btn-sm btn-light float-right' onClick={onClickNext}>{t('Next')}</button>)}
+            </div>
+        </div>
+    );
+};
+
+
+export default UserList;
