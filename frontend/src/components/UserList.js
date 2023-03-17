d052eeb19c3883a0816146031faa2f193f4f828b
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-17 23:42:45 +0300

    added spinner according to pendingApiCall

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { getUsers } from '../api/apiCalls';
 import { useTranslation } from 'react-i18next';
 import UserListItem from './UserListItem';
+import { useApiProgress } from '../shared/ApiProgress';
 
 const UserList = () => {
 
+    const [loadFailure, setLoadFailure] = useState(false);
     const [page, setPage] = useState({
         content: [],
         number: 0,
         size: 3
     });
+
+    const pendingApiCall = useApiProgress("/api/1.0/users?page");
 
     useEffect(() => {
         loadUsers();
@@ -25,14 +29,36 @@ const UserList = () => {
         loadUsers(previousPage);
     }
 
-    const loadUsers = page => {
-        getUsers(page).then(response => {
+    const loadUsers = async page => {
+        setLoadFailure(false);
+        try {
+            const response = await getUsers(page);
             setPage(response.data);
-        });
-    }
+        } catch (error) {
+            setLoadFailure(true);
+        }
 
+    }
     const { content: users, last, first } = page;
     const { t } = useTranslation();
+    let actionDiv = (
+        <div>
+            {first === false && (<button className='btn btn-sm btn-light' onClick={onClickPrevious}>{t('Back')}</button>)}
+
+            {last === false && (<button className='btn btn-sm btn-light float-right' onClick={onClickNext}>{t('Next')}</button>)}
+        </div>
+    );
+
+    if (pendingApiCall) {
+        actionDiv = (
+            <div className='d-flex justify-content-center'>
+                <div className='spinner-border text-black-50'>
+                    <span className='sr-only' />
+                </div>
+            </div>
+        );
+    };
+
     return (
 
         <div className='card'>
@@ -44,11 +70,8 @@ const UserList = () => {
                     ))
                 }
             </div>
-            <div>
-                {first === false && (<button className='btn btn-sm btn-light' onClick={onClickPrevious}>{t('Back')}</button>)}
-
-                {last === false && (<button className='btn btn-sm btn-light float-right' onClick={onClickNext}>{t('Next')}</button>)}
-            </div>
+            {actionDiv}
+            {loadFailure && <div className='text-center text-danger'>{t('Load Failure')}</div>}
         </div>
     );
 };
