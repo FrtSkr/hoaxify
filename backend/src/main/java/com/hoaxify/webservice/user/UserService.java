262c0dfca34da1a4b67d141be575803be69037b8
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-20 18:35:28 +0300

    created getByUsername method

--- backend/src/main/java/com/hoaxify/webservice/user/UserService.java
+++ backend/src/main/java/com/hoaxify/webservice/user/UserService.java
@@ -1,5 +1,6 @@
 package com.hoaxify.webservice.user;
 
+import com.hoaxify.webservice.error.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,5 +29,13 @@ public class UserService {
             return userRepository.findByUsernameNot(user.getUsername(), page);
         }
         return userRepository.findAll(page);
+    }
+
+    public User getByUsername(String username) {
+        User inDb = userRepository.findByUsername(username);
+        if (inDb == null){
+            throw new NotFoundException("User Not Found", "/api/1.0/users/{username}");
+        }
+        return inDb;
     }
 }
