c8366c5536bd2b40cf9c20b252037a407344ca24
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-20 20:46:02 +0300

    show error on user side

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -2,27 +2,44 @@ import React, { useEffect, useState } from 'react';
 import ProfileCard from '../components/ProfileCard';
 import { getUser } from '../api/apiCalls';
 import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 
 const UserPage = props => {
     const [user, setUser] = useState();
+    const [notFound, setNotFound] = useState(false);
     const { username } = useParams();
+    const { t } = useTranslation();
     useEffect(() => {
         const loadUser = async () => {
             try {
                 const response = await getUser(username);
                 setUser(response.data);
+                setNotFound(false);
             } catch (error) {
-
+                setNotFound(true);
             }
         };
         loadUser();
     }, [username]);
 
     return (
-        <div className='container'>
-            <ProfileCard />
-        </div>
+        !notFound ?
+            <div className='container'>
+                <ProfileCard />
+            </div> :
+
+
+            <div className='container'>
+                <div className='alert alert-danger text-center'>
+                    <div>
+                        <span class="material-symbols-sharp" style={{ fontSize: '48px' }}>
+                            report
+                        </span>
+                    </div>
+                    {t('User not found')}
+                </div>
+            </div>
     );
 };
 
