dbfbfc7385b4f9673c008ea43b75b7588bc85d27
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-20 19:03:45 +0300

    taken user

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProfileCard from '../components/ProfileCard';
+import { getUser } from '../api/apiCalls';
+import { useParams } from 'react-router-dom';
 
-const UserPage = () => {
+
+const UserPage = props => {
+    const [user, setUser] = useState();
+    const { username } = useParams();
+    useEffect(() => {
+        const loadUser = async () => {
+            try {
+                const response = await getUser(username);
+                setUser(response.data);
+            } catch (error) {
+
+            }
+        };
+        loadUser();
+    }, [username]);
+
     return (
         <div className='container'>
             <ProfileCard />
