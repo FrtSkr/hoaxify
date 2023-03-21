8449a5f6c8aad712c78a4c29cbe14a586f7fc7d2
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-21 18:48:56 +0300

    created spinner component

---
+++ frontend/src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Spinner = _ => {
+    return (
+        <div className='d-flex justify-content-center'>
+            <div className='spinner-border text-black-50'>
+                <span className='sr-only' />
+            </div>
+        </div>
+
+    );
+};
