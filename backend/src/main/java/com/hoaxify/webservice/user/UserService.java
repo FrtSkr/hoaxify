ffc1d05ddf17145e8f1204e59eaf7b1491381eeb
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-26 14:52:22 +0300

    convert user image from string to file

--- backend/src/main/java/com/hoaxify/webservice/user/UserService.java
+++ backend/src/main/java/com/hoaxify/webservice/user/UserService.java
@@ -1,6 +1,7 @@
 package com.hoaxify.webservice.user;
 
 import com.hoaxify.webservice.error.NotFoundException;
+import com.hoaxify.webservice.file.FileService;
 import com.hoaxify.webservice.user.vm.UserUpdateVM;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,14 +10,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
+import java.io.IOException;
+
+
+
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
 
     PasswordEncoder passwordEncoder;
-    public UserService(PasswordEncoder passwordEncoder) {
+
+    FileService fileService;
+    public UserService(PasswordEncoder passwordEncoder, FileService fileService) {
+
         this.passwordEncoder = passwordEncoder;
+        this.fileService = fileService;
     }
 
 
@@ -45,8 +54,16 @@ public class UserService {
         User inDb = getByUsername(username);
         inDb.setDisplayName(updatedUser.getDisplayName());
         if(updatedUser.getImage() != null){
-            inDb.setImage(updatedUser.getImage());
+            try{
+                String storedFileName = fileService.writeBase64EncodedStringToFile(updatedUser.getImage());
+                inDb.setImage(storedFileName);
+            }catch (IOException e){
+                e.printStackTrace();;
+            }
+
         }
         return userRepository.save(inDb);
     }
+
+
 }
