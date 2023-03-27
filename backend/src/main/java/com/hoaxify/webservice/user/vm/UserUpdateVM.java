540adf818ae4196005614ceaa0ee858bb3dd26ce
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-27 17:54:38 +0300

    added validation constraint for displayName

--- backend/src/main/java/com/hoaxify/webservice/user/vm/UserUpdateVM.java
+++ backend/src/main/java/com/hoaxify/webservice/user/vm/UserUpdateVM.java
@@ -1,9 +1,11 @@
 package com.hoaxify.webservice.user.vm;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserUpdateVM {
+    @Size(min = 4, max = 255, message = "{hoaxify.constraint.Size.message}")
     private String displayName;
     private String image;
 }
