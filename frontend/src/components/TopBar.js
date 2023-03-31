3a0f9def3d1628ac0f3d8ed28ae3b175a69af658
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-01 00:19:25 +0300

    added language selector

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -6,6 +6,7 @@ import logo from '../assets/hoaxify.png';
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutSuccess } from '../redux/authActions';
 import { ProfileImageWithDefault } from './ProfileImageWithDefault';
+import LanguageSelector from './LanguageSelector';
 
 const TopBar = props => {
 
@@ -58,29 +59,36 @@ const TopBar = props => {
         if (menuVisible) {
             dropdownMenuClassName += ' show';
         }
-        links = (<ul className="navbar-nav ms-auto">
-            <li className='nav-item dropdown'>
-                <div className='d-flex' style={{ cursor: 'pointer' }} onClick={() => { setMenuVisible(!menuVisible) }} ref={menuArea}>
-                    <ProfileImageWithDefault className="rounded-circle m-auto" image={image} width="32" height="32" />
-                    <span className='nav-link dropdown-toggle'>{displayName}</span>
-                </div>
-                <div className={dropdownMenuClassName}>
-                    <Link className='dropdown-item d-flex p-2' to={`/user/${username}`}>
-                        <span className="material-symbols-outlined text-info ms-2">
-                            person
-                        </span>
-                        {t('Profile')}
-                    </Link>
+        links = (
+            <div className='d-flex ms-auto'>
+                <ul className="navbar-nav">
+                    <li className='nav-item dropdown'>
+                        <div className='d-flex' style={{ cursor: 'pointer' }} onClick={() => { setMenuVisible(!menuVisible) }} ref={menuArea}>
+                            <ProfileImageWithDefault className="rounded-circle m-auto" image={image} width="32" height="32" />
+                            <span className='nav-link dropdown-toggle'>{displayName}</span>
+                        </div>
+                        <div className={dropdownMenuClassName}>
+                            <Link className='dropdown-item d-flex p-2' to={`/user/${username}`}>
+                                <span className="material-symbols-outlined text-info ms-2">
+                                    person
+                                </span>
+                                {t('Profile')}
+                            </Link>
 
-                    <span className='dropdown-item d-flex p-2' onClick={onLogoutSuccess} style={{ cursor: 'pointer' }}>
-                        <span className="material-symbols-outlined text-danger ms-2">
-                            power_settings_new
-                        </span>
-                        {t('Logout')}
-                    </span>
+                            <span className='dropdown-item d-flex p-2' onClick={onLogoutSuccess} style={{ cursor: 'pointer' }}>
+                                <span className="material-symbols-outlined text-danger ms-2">
+                                    power_settings_new
+                                </span>
+                                {t('Logout')}
+                            </span>
+
+                        </div>
+                    </li>
+                </ul>
+                <div className='m-2'>
+                    <LanguageSelector />
                 </div>
-            </li>
-        </ul>);
+            </div>);
     }
     return (
         <div className="shadow-sm bg-light mb-2">
