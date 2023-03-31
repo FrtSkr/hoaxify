09b215066b61578e1f2ee958707427c30d92c386
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-31 23:24:16 +0300

    created hoax controller and added save hoax method

---
+++ backend/src/main/java/com/hoaxify/webservice/hoax/HoaxController.java
@@ -0,0 +1,21 @@
+package com.hoaxify.webservice.hoax;
+
+import com.hoaxify.webservice.shared.GenericResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/1.0")
+public class HoaxController {
+
+    @Autowired
+    HoaxService hoaxService;
+
+    @PostMapping("/hoaxes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public GenericResponse saveHoax(@RequestBody Hoax hoax){
+        hoaxService.save(hoax);
+        return new GenericResponse("Hoax created");
+    }
+}
