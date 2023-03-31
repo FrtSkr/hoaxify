da0953b2e05fb1a6b74aa9fbb319f4b20f286379
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-03-31 22:12:27 +0300

    added hoaxify area next to user lists

--- frontend/src/pages/HomePages.js
+++ frontend/src/pages/HomePages.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import UserList from '../components/UserList';
+import HoaxSubmit from '../components/HoaxSubmit';
+import { useSelector } from 'react-redux';
 const HomePages = () => {
+    const { isLoggedIn } = useSelector(store => ({ isLoggedIn: store.isLoggedIn }));
+
+    let userListClassName = 'col-6';
+    if (isLoggedIn == false) {
+        userListClassName = 'col-12';
+    }
     return (
         <div className='container'>
-            <UserList />
+            <div className='row'>
+                <div className='col-6'>
+                    {isLoggedIn && <HoaxSubmit />}
+                </div>
+                <div className={userListClassName}>
+                    <UserList />
+
+                </div>
+            </div>
         </div>
     );
 };
