d163b78e831c36141cb9c34bb5e49a813c9f4765
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-01 13:37:52 +0300

    added HoaxFeed component for show hoaxes

--- frontend/src/pages/HomePages.js
+++ frontend/src/pages/HomePages.js
@@ -2,6 +2,7 @@ import React from 'react';
 import UserList from '../components/UserList';
 import HoaxSubmit from '../components/HoaxSubmit';
 import { useSelector } from 'react-redux';
+import HoaxFeed from '../components/HoaxFeed';
 const HomePages = () => {
     const { isLoggedIn } = useSelector(store => ({ isLoggedIn: store.isLoggedIn }));
 
@@ -13,7 +14,13 @@ const HomePages = () => {
         <div className='container'>
             <div className='row'>
                 <div className='col-6'>
-                    {isLoggedIn && <HoaxSubmit />}
+                    {
+                        isLoggedIn &&
+                        <div className='mb-1'>
+                            <HoaxSubmit />
+                        </div>
+                    }
+                    <HoaxFeed />
                 </div>
                 <div className={userListClassName}>
                     <UserList />
