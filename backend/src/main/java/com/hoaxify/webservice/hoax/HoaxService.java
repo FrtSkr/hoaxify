d58560aeb82bef0c6e73ab8163f64560aed698c8
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-01 12:44:35 +0300

    added getHoaxes method

--- backend/src/main/java/com/hoaxify/webservice/hoax/HoaxService.java
+++ backend/src/main/java/com/hoaxify/webservice/hoax/HoaxService.java
@@ -1,6 +1,8 @@
 package com.hoaxify.webservice.hoax;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +12,9 @@ public class HoaxService {
     HoaxRepository hoaxRepository;
     public void save(Hoax hoax) {
         hoaxRepository.save(hoax);
+    }
+
+    public Page<Hoax> getHoaxes(Pageable page) {
+        return hoaxRepository.findAll(page);
     }
 }
