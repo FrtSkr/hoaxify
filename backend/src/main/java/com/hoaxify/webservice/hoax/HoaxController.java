8cd6fb6229559bd656052f16f43722cc6bbaac4d
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-01 12:44:08 +0300

    added getHoaxes method

--- backend/src/main/java/com/hoaxify/webservice/hoax/HoaxController.java
+++ backend/src/main/java/com/hoaxify/webservice/hoax/HoaxController.java
@@ -3,6 +3,8 @@ package com.hoaxify.webservice.hoax;
 import com.hoaxify.webservice.shared.GenericResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,5 +20,10 @@ public class HoaxController {
     public GenericResponse saveHoax(@Valid @RequestBody Hoax hoax){
         hoaxService.save(hoax);
         return new GenericResponse("Hoax created");
+    }
+
+    @GetMapping("/hoaxes")
+    public Page<Hoax> getHoaxes(Pageable page){
+        return hoaxService.getHoaxes(page);
     }
 }
