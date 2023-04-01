a9ff26de7b2e5801e92348e02a6ef0018a8a5917
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-01 13:36:21 +0300

    added getHoaxes request method and created domain enums

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -1,13 +1,18 @@
 import axios from "axios"; // for HTTP request
+const enumDomainName = {
+    users: 'users',
+    auth: 'auth',
+    hoaxes: 'hoaxes'
 
+}
 const baseUrl = '/api/1.0/'
 export const signup = body => {
-    const url = baseUrl + 'users';
+    const url = baseUrl + enumDomainName.users;
     return axios.post(url, body);
 };
 
 export const login = creds => {
-    const url = baseUrl + 'auth';
+    const url = baseUrl + enumDomainName.auth;
     return axios.post(url, {}, { auth: creds });
 }
 
@@ -17,7 +22,7 @@ export const changeLanguage = language => {
 
 
 export const getUsers = (page = 0, size = 3) => {
-    return axios.get(baseUrl + `users?page=${page}&size=${size}`);
+    return axios.get(baseUrl + `${enumDomainName.users}?page=${page}&size=${size}`);
 }
 
 export const setAuthorizationHeader = ({ username, password, isLoggedIn }) => {
@@ -30,13 +35,17 @@ export const setAuthorizationHeader = ({ username, password, isLoggedIn }) => {
 };
 
 export const getUser = username => {
-    return axios.get(`${baseUrl}users/${username}`);
+    return axios.get(`${baseUrl}${enumDomainName.users}/${username}`);
 };
 
 export const updateUser = (username, body) => {
-    return axios.put(`${baseUrl}users/${username}`, body);
+    return axios.put(`${baseUrl}${enumDomainName.users}/${username}`, body);
 }
 
 export const postHoax = hoax => {
-    return axios.post(`${baseUrl}hoaxes`, hoax);
+    return axios.post(`${baseUrl}${enumDomainName.hoaxes}`, hoax);
+}
+
+export const getHoaxes = () => {
+    return axios.get(`${baseUrl}${enumDomainName.hoaxes}`);
 }
