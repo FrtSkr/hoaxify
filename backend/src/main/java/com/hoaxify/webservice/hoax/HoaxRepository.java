9cbdaaf768f31596a0ab7402e4f94e180e57c901
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-05 18:26:49 +0300

    added findByUser method for hoaxes of user

--- backend/src/main/java/com/hoaxify/webservice/hoax/HoaxRepository.java
+++ backend/src/main/java/com/hoaxify/webservice/hoax/HoaxRepository.java
@@ -1,6 +1,10 @@
 package com.hoaxify.webservice.hoax;
 
+import com.hoaxify.webservice.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HoaxRepository extends JpaRepository<Hoax, Long> {
+    Page<Hoax> findByUser(User user, Pageable page);
 }
