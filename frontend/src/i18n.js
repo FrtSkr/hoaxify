fc7757dfdfe365a49b265984b85da1888c5c52c4
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-05 17:57:32 +0300

    added language select for timeago

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
+import { register } from 'timeago.js';
 i18n.use(initReactI18next).init({
     resources: {
         en: {
@@ -22,7 +22,9 @@ i18n.use(initReactI18next).init({
                 Cancel: 'Cancel',
                 'Change Display Name': 'Change Display Name',
                 Users: 'Users',
-                Profile: 'Profile'
+                Profile: 'Profile',
+                'There are no hoaxes': 'There are no hoaxes',
+                'Load old hoaxes': 'Load old Hoaxes'
             }
         },
         tr: {
@@ -44,7 +46,9 @@ i18n.use(initReactI18next).init({
                 Cancel: 'İptal Et',
                 'Change Display Name': 'Görünür İsminizi Değiştirin',
                 Users: 'Kullanıcılar',
-                Profile: "Hesap"
+                Profile: "Hesap",
+                'There are no hoaxes': 'Hiç hoax bulunamadı',
+                'Load old hoaxes': 'Geçmiş Hoaxları Getir'
             }
         }
     },
@@ -61,4 +65,24 @@ i18n.use(initReactI18next).init({
     }
 });
 
+
+const timeagoTR = (number, index) => {
+    return [
+        ["az önce", "şimdi"],
+        ["%s saniye önce", "%s saniye içinde"],
+        ["1 dakika önce", "1 dakika içinde"],
+        ["%s dakika önce", "%s dakika içinde"],
+        ["1 saat önce", "1 saat içinde"],
+        ["%s saat önce", "%s saat içinde"],
+        ["1 gün önce", "1 gün içinde"],
+        ["%s gün önce", "%s gün içinde"],
+        ["1 hafta önce", "1 hafta içinde"],
+        ["%s hafta önce", "%s hafta içinde"],
+        ["1 ay önce", "1 ay içinde"],
+        ["%s ay önce", "%s ay içinde"],
+        ["1 yıl önce", "1 yıl içinde"],
+        ["%s yıl önce", "%s yıl içinde"]
+    ][index]
+};
+register('tr', timeagoTR);
 export default i18n;
