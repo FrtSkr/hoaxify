5c5774d6a0ff6a7aa1bf93e842bd8f7fc5bff925
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-05 18:27:58 +0300

    added getUserHoaxes method for hoaxes of user

--- backend/src/main/java/com/hoaxify/webservice/hoax/HoaxController.java
+++ backend/src/main/java/com/hoaxify/webservice/hoax/HoaxController.java
@@ -31,4 +31,10 @@ public class HoaxController {
     public Page<HoaxVM> getHoaxes(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable page){
         return hoaxService.getHoaxes(page).map(HoaxVM::new);
     }
+
+    @GetMapping("/users/{username}/hoaxes")
+    public Page<HoaxVM> getUserHoaxes(@PathVariable String username, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable page){
+        return hoaxService.getHoaxesOfUser(username, page).map(HoaxVM::new);
+    }
+
 }
