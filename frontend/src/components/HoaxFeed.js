38717042400e2f780eda4f8813f51cd5c6c9ed58
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-05 19:18:59 +0300

    fixed api progress request path parameter and getHoaxes method parameter

--- frontend/src/components/HoaxFeed.js
+++ frontend/src/components/HoaxFeed.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { getHoaxes } from '../api/apiCalls';
+import { getHoaxes, enumDomainName } from '../api/apiCalls';
 import { useTranslation } from 'react-i18next';
 import HoaxView from './HoaxView';
 import { useApiProgress } from '../shared/ApiProgress';
 import { Spinner } from './Spinner';
+import { useParams } from 'react-router-dom';
 
 const HoaxFeed = props => {
     const [hoaxPage, setHoaxPage] = useState({ content: [], last: true, number: 0 });
     const { t } = useTranslation();
-    const pendingApiCall = useApiProgress('get', '/api/1.0/hoaxes');
+    const { username } = useParams();
+    const path = username ? `/api/1.0/${enumDomainName.users}/${username}/${enumDomainName.hoaxes}?page=`
+        : `/api/1.0/${enumDomainName.hoaxes}?page=`;
+    const pendingApiCall = useApiProgress('get', path);
 
     useEffect(() => {
         loadHoaxes();
@@ -16,7 +20,7 @@ const HoaxFeed = props => {
 
     const loadHoaxes = async (page) => {
         try {
-            const response = await getHoaxes(page);
+            const response = await getHoaxes(username, page);
             setHoaxPage(previousHoaxPage => ({
                 ...response.data,
                 content: [...previousHoaxPage.content, ...response.data.content]
