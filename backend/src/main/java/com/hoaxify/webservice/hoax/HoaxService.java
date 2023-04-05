939ddf464e130d78bc37bc3749e59b17c352328d
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-05 18:29:14 +0300

    added getHoaxesOfUser method for hoaxes of user

--- backend/src/main/java/com/hoaxify/webservice/hoax/HoaxService.java
+++ backend/src/main/java/com/hoaxify/webservice/hoax/HoaxService.java
@@ -1,16 +1,22 @@
 package com.hoaxify.webservice.hoax;
 
 import com.hoaxify.webservice.user.User;
+import com.hoaxify.webservice.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class HoaxService {
 
     @Autowired
     HoaxRepository hoaxRepository;
+
+    @Autowired
+    UserService userService;
     public void save(Hoax hoax, User user) {
         hoax.setUser(user);
         hoaxRepository.save(hoax);
@@ -18,5 +24,10 @@ public class HoaxService {
 
     public Page<Hoax> getHoaxes(Pageable page) {
         return hoaxRepository.findAll(page);
+    }
+
+    public Page<Hoax> getHoaxesOfUser(String username, Pageable page) {
+        User inDB= userService.getByUsername(username);
+        return hoaxRepository.findByUser(inDB, page);
     }
 }
