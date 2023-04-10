4927255274f34a96ff160526d996cce6a67e9a44
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-10 22:38:22 +0300

    added findByIdLessThanAndUser method

--- backend/src/main/java/com/hoaxify/webservice/hoax/HoaxRepository.java
+++ backend/src/main/java/com/hoaxify/webservice/hoax/HoaxRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HoaxRepository extends JpaRepository<Hoax, Long> {
     Page<Hoax> findByUser(User user, Pageable page);
-    Page<Hoax> findByIdLessThan(long id, Pageable pageable);
+    Page<Hoax> findByIdLessThan(long id, Pageable page);
+
+    Page<Hoax> findByIdLessThanAndUser(long id, User user, Pageable page);
 }
