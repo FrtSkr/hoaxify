5d7a1bf489f02867c6d81ffd754f11dd8ed598b3
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-11 17:26:49 +0300

    fixed by to count of new hoaxes with setInterval method

--- frontend/src/components/HoaxFeed.js
+++ frontend/src/components/HoaxFeed.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getHoaxes, enumDomainName, getOldHoaxes } from '../api/apiCalls';
+import { getHoaxes, enumDomainName, getOldHoaxes, getNewHoaxCount } from '../api/apiCalls';
 import { useTranslation } from 'react-i18next';
 import HoaxView from './HoaxView';
 import { useApiProgress } from '../shared/ApiProgress';
@@ -8,6 +8,7 @@ import { useParams } from 'react-router-dom';
 
 const HoaxFeed = props => {
     const [hoaxPage, setHoaxPage] = useState({ content: [], last: true, number: 0 });
+    const [newHoaxCount, setNewNoaxCount] = useState();
     const { t } = useTranslation();
     const { username } = useParams();
     const path = username ? `/api/1.0/${enumDomainName.users}/${username}/${enumDomainName.hoaxes}?page=`
@@ -15,12 +16,31 @@ const HoaxFeed = props => {
     const initialHoaxLoadProgress = useApiProgress('get', path);
 
     let lastHoaxId = 0;
+    let firstHoaxId = 0;
     if (hoaxPage.content.length > 0) {
+        firstHoaxId = hoaxPage.content[0].id;
         const lastHoaxIndex = hoaxPage.content.length - 1;
         lastHoaxId = hoaxPage.content[lastHoaxIndex].id;
     }
     const oldHoaxPath = username ? `/api/1.0/${enumDomainName.users}/${username}/${enumDomainName.hoaxes}/${lastHoaxId}` : `/api/1.0/${enumDomainName.hoaxes}/${lastHoaxId}`
     const loadOldHoaxesProgress = useApiProgress('get', oldHoaxPath, true);
+
+    useEffect(() => {
+        const getCount = async () => {
+            try {
+                const response = await getNewHoaxCount(firstHoaxId);
+                setNewNoaxCount(response.data.count);
+            } catch (error) {
+
+            }
+        }
+        let looper = setInterval(() => {
+            getCount();
+        }, 10000);
+        return function cleanup() {
+            clearInterval(looper);
+        };
+    }, [firstHoaxId]);
 
     useEffect(() => {
         const loadHoaxes = async (page) => {
@@ -63,6 +83,9 @@ const HoaxFeed = props => {
 
     return (
         <div>
+            {newHoaxCount > 0 && <div className='alert alert-secondary text-center mb-1'>
+                {t('There are new hoaxes')}
+            </div>}
             {content.map(hoax => {
                 return <HoaxView key={hoax.id} hoax={hoax} />
             })}
