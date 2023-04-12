164b3113aed475a320c9358d20eea988291a21b8
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-12 18:46:46 +0300

    concatenated method of user to generic hoax method

--- backend/src/main/java/com/hoaxify/webservice/hoax/HoaxController.java
+++ backend/src/main/java/com/hoaxify/webservice/hoax/HoaxController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/1.0")
@@ -33,54 +32,32 @@ public class HoaxController {
         return new GenericResponse("Hoax created");
     }
 
-    @GetMapping("/hoaxes")
-    public Page<HoaxVM> getHoaxes(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable page){
-        return hoaxService.getHoaxes(page).map(HoaxVM::new);
+    @GetMapping({"/hoaxes", "/users/{username}/hoaxes"})
+    public Page<HoaxVM> getHoaxes(@PathVariable(required = false) String username, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable page){
+        return hoaxService.getHoaxes(username, page).map(HoaxVM::new);
     }
 
-    @GetMapping("/users/{username}/hoaxes")
-    public Page<HoaxVM> getUserHoaxes(@PathVariable String username, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable page){
-        return hoaxService.getHoaxesOfUser(username, page).map(HoaxVM::new);
-    }
 
-    @GetMapping("/hoaxes/{id:[0-9]+}")
+    @GetMapping({"/hoaxes/{id:[0-9]+}", "/users/{username}/hoaxes/{id:[0-9]+}"})
     public ResponseEntity<?> getHoaxesRelative(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable page,
                                           @PathVariable long id,
+                                               @PathVariable(required = false) String username,
                                                @RequestParam(name = "count", required = false, defaultValue = "false") boolean count,
                                                @RequestParam(name = "direction", defaultValue = "before") String direction
                                                ){
         if(count){
-            long newHoaxCount = hoaxService.getNewHoaxesCount(id);
+            long newHoaxCount = hoaxService.getNewHoaxesCount(id, username);
             Map<String, Long> response = new HashMap<>();
             response.put("count", newHoaxCount);
             return ResponseEntity.ok(response);
         }
         if(direction.equals("after")){
 
-            List<HoaxVM> newHoaxes = hoaxService.getNewHoaxes(id, page.getSort())
+            List<HoaxVM> newHoaxes = hoaxService.getNewHoaxes(id, username, page.getSort())
                     .stream().map(HoaxVM::new).toList();
             return ResponseEntity.ok(newHoaxes);
         }
-        return ResponseEntity.ok(hoaxService.getOldHoaxes(id, page).map(HoaxVM::new));
-    }
-
-    @GetMapping("/users/{username}/hoaxes/{id:[0-9]+}")
-    public ResponseEntity<?> getUserHoaxesRelative(@PathVariable long id, @PathVariable String username,
-                                              @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable page,
-                                              @RequestParam(name = "count", required = false, defaultValue = "false") boolean count,
-                                                   @RequestParam(name = "direction", defaultValue = "before") String direction){
-        if(count){
-            long newHoaxCount = hoaxService.getNewHoaxesCountOfUser(id, username);
-            Map<String, Long> response = new HashMap<>();
-            response.put("count", newHoaxCount);
-            return ResponseEntity.ok(response);
-        }
-        if(direction.equals("after")){
-            List<HoaxVM> newHoaxesOfUser = hoaxService.getNewHoaxesOfUser(id, username, page.getSort())
-                    .stream().map(HoaxVM::new).toList();
-            return ResponseEntity.ok(newHoaxesOfUser);
-        }
-        return ResponseEntity.ok(hoaxService.getOldHoaxesOfUser(id, username, page).map(HoaxVM::new));
+        return ResponseEntity.ok(hoaxService.getOldHoaxes(id, username, page).map(HoaxVM::new));
     }
 
 }
