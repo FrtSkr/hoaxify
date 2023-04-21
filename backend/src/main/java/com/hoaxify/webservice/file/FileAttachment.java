949360d22dcb02a4c4867fdea4038ffec019d493
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-21 13:58:08 +0300

    created entity for user attachment

---
+++ backend/src/main/java/com/hoaxify/webservice/file/FileAttachment.java
@@ -0,0 +1,26 @@
+package com.hoaxify.webservice.file;
+
+import com.hoaxify.webservice.hoax.Hoax;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+public class FileAttachment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    @OneToOne
+    private Hoax hoax;
+
+
+}
