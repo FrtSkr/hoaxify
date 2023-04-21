409e7118d13e14e2fb827fd06c4015e2ed957c6e
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-21 15:44:48 +0300

    added FileAttachmentVM field and set in constructor

--- backend/src/main/java/com/hoaxify/webservice/hoax/vm/HoaxVM.java
+++ backend/src/main/java/com/hoaxify/webservice/hoax/vm/HoaxVM.java
@@ -1,4 +1,6 @@
 package com.hoaxify.webservice.hoax.vm;
+import com.hoaxify.webservice.file.FileAttachment;
+import com.hoaxify.webservice.file.vm.FileAttachmentVM;
 import com.hoaxify.webservice.hoax.Hoax;
 import com.hoaxify.webservice.user.vm.UserVM;
 import lombok.Data;
@@ -14,10 +16,15 @@ public class HoaxVM {
 
     private UserVM user;
 
+    private FileAttachmentVM fileAttachment;
+
     public HoaxVM(Hoax hoax){
         this.setId(hoax.getId());
         this.setContent(hoax.getContent());
         this.setTimestamp(hoax.getTimestamp().getTime());
         this.setUser(new UserVM(hoax.getUser()));
+        if(hoax.getFileAttachment() != null){
+            this.setFileAttachment(new FileAttachmentVM(hoax.getFileAttachment()));
+        }
     }
 }
