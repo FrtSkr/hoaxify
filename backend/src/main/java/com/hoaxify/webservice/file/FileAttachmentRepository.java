a8964ae99b41b5d37907d25eb2d1daa0feadbc6b
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-21 13:58:31 +0300

    created attachment repository

---
+++ backend/src/main/java/com/hoaxify/webservice/file/FileAttachmentRepository.java
@@ -0,0 +1,7 @@
+package com.hoaxify.webservice.file;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long> {
+
+}
