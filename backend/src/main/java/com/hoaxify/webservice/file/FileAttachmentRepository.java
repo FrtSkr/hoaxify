a1aee76da562c48c182454d80d183faacd31d042
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-21 14:35:09 +0300

    added method for find attachments by date before and null hoax

--- backend/src/main/java/com/hoaxify/webservice/file/FileAttachmentRepository.java
+++ backend/src/main/java/com/hoaxify/webservice/file/FileAttachmentRepository.java
@@ -2,6 +2,9 @@ package com.hoaxify.webservice.file;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long> {
+import java.util.Date;
+import java.util.List;
 
+public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long> {
+    List<FileAttachment> findByDateBeforeAndHoaxIsNull(Date date);
 }
