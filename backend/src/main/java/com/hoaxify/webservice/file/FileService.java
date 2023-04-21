fb97589ba7043394b98bbed3a42355a731045abc
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-04-21 14:37:57 +0300

    added scheduled method for cleanup of hoaxes outside relation

--- backend/src/main/java/com/hoaxify/webservice/file/FileService.java
+++ backend/src/main/java/com/hoaxify/webservice/file/FileService.java
@@ -2,6 +2,8 @@ package com.hoaxify.webservice.file;
 
 import com.hoaxify.webservice.configuration.AppConfiguration;
 import org.apache.tika.Tika;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,9 +15,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Service
+@EnableScheduling
 public class FileService {
 
 
@@ -79,5 +83,17 @@ public class FileService {
         attachment.setDate(new Date());
 
         return fileAttachmentRepository.save(attachment);
+    }
+
+    @Scheduled(fixedRate = 24 * 60 * 60 * 1000)
+    public void cleanupStorage(){
+        Date twentyFourHoursAgo = new Date(System.currentTimeMillis() - (24 * 60 * 60 * 1000));
+        List<FileAttachment> filesToBeDeleted = fileAttachmentRepository.findByDateBeforeAndHoaxIsNull(twentyFourHoursAgo);
+        for(FileAttachment file : filesToBeDeleted){
+            // delete file
+            deleteFile(file.getName());
+            // delete from table
+            fileAttachmentRepository.deleteById(file.getId());
+        }
     }
 }
