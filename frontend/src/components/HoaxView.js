359567202e11c6f3b4a6b0ba00f704bbe8b16aea
FIRAT ŞAKAR <frtskr0@gmail.com>
2023-05-01 22:04:17 +0300

    added onDeleteHoax prop for remove hoax

--- frontend/src/components/HoaxView.js
+++ frontend/src/components/HoaxView.js
@@ -4,13 +4,24 @@ import { Link } from 'react-router-dom';
 import { format } from 'timeago.js';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
+import { deleteHoax } from '../api/apiCalls';
 const HoaxView = (props) => {
     const loggedInUser = useSelector(store => store.username);
-    const { hoax } = props;
-    const { user, content, timestamp, fileAttachment } = hoax;
+    const { hoax, onDeleteHoax } = props;
+    const { user, content, timestamp, fileAttachment, id } = hoax;
     const { username, displayName, image } = user;
 
     const { i18n } = useTranslation();
+
+    const onClickDelete = async () => {
+        try {
+            const response = await deleteHoax(id);
+            onDeleteHoax(id);
+        } catch (error) {
+
+        }
+    }
+
     const formatted = format(timestamp, i18n.language);
 
     const ownedByLoggedInUser = loggedInUser == username;
@@ -26,7 +37,7 @@ const HoaxView = (props) => {
                         </h6>
                     </Link>
                 </div>
-                {ownedByLoggedInUser && <button className="btn btn-delete-link btn-sm">
+                {ownedByLoggedInUser && <button className="btn btn-delete-link btn-sm" onClick={onClickDelete}>
                     <span class="material-symbols-outlined">
                         delete
                     </span>
