d94e6ff0cca0a91aaeb50b1db2a54c4d53b5123e
Fırat Şakar <frtskr0@gmail.com>
2023-05-07 23:41:57 +0300

    fixed deleteUser method

--- backend/src/main/java/com/hoaxify/webservice/user/UserService.java
+++ backend/src/main/java/com/hoaxify/webservice/user/UserService.java
@@ -83,7 +83,6 @@ public class UserService {
 
     public void deleteUser(String username) {
         hoaxService.deleteHoaxesOfUser(username);
-        User inDB = userRepository.findByUsername(username);
-        userRepository.delete(inDB);
+        userRepository.deleteByUserName(username);
     }
 }
