3c0af6bb8deba686d688c0fae0546758a42640cd
Fırat Şakar <frtskr0@gmail.com>
2023-05-07 23:41:30 +0300

    added deleteByUserName method with Transactional annotation

--- backend/src/main/java/com/hoaxify/webservice/user/UserRepository.java
+++ backend/src/main/java/com/hoaxify/webservice/user/UserRepository.java
@@ -1,4 +1,5 @@
 package com.hoaxify.webservice.user;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
      // UserProjection için kullanılan metot. Ama biz farklı bir yöntem uygulayacağız.
 //     @Query(value = "Select u from User u")
 //     Page<UserProjection> getAllUsersProjection(Pageable page);
+
+     @Transactional
+     void deleteByUserName(String username);
 }
