e1ffe7e6176650330b6079acc2e849189359c0cc
Fırat Şakar <frtskr0@gmail.com>
2023-05-17 23:55:37 +0300

    fixed handleAuthentication method for JWT

--- backend/src/main/java/com/hoaxify/webservice/auth/AuthController.java
+++ backend/src/main/java/com/hoaxify/webservice/auth/AuthController.java
@@ -1,17 +1,19 @@
 package com.hoaxify.webservice.auth;
-import com.hoaxify.webservice.shared.CurrentUser;
-import com.hoaxify.webservice.user.User;
-import com.hoaxify.webservice.user.vm.UserVM;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
-    final String basePath = "/api/1.0/auth";
+    private AuthService authService;
 
-    @PostMapping(basePath)
-    UserVM handleAuthentication(@CurrentUser User user){
-        return new UserVM(user);
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+    @PostMapping("/api/1.0/auth")
+    AuthResponse handleAuthentication(@RequestBody Credentials credentials){
+        return authService.authenticate(credentials);
     }
 
 }
