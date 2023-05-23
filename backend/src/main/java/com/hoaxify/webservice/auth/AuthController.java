f47c2c45f496562047ea3ddd3320dadbefca14f6
Fırat Şakar <frtskr0@gmail.com>
2023-05-23 17:36:18 +0300

    created handleLogout method for clear token

--- backend/src/main/java/com/hoaxify/webservice/auth/AuthController.java
+++ backend/src/main/java/com/hoaxify/webservice/auth/AuthController.java
@@ -1,7 +1,9 @@
 package com.hoaxify.webservice.auth;
 
+import com.hoaxify.webservice.shared.GenericResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +17,14 @@ public class AuthController {
     AuthResponse handleAuthentication(@RequestBody Credentials credentials){
         return authService.authenticate(credentials);
     }
+
+    @PostMapping("/api/1.0/logout")
+    GenericResponse handleLogout(@RequestHeader(name = "Authorization") String authorization){
+        String token = authorization.substring(7);
+        authService.clearToken(token);
+        return new GenericResponse("Logout success");
+    }
+
+
 
 }
